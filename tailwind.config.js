93cc87ebd4ad868fe99cb2afe090cd8f09e79160
Chaion Saha <chaionsaha00@gmail.com>
2023-11-14 12:10:23 +0600

    homepage completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@
 module.exports = {
 	content: ['./**/*.{html,js}'],
 	theme: {
-		colors: {
-			primary: '#ef3b2e',
-			'parimary-subtile': '#ffccba',
-			'bg-base-100': '#fff',
-			'bg-base-200': '#f2f3f4',
+		extend: {
+			colors: {
+				primary: '#ef3b2e',
+				'primary-subtle': '#ffccba',
+				'base-100': '#fff',
+				'base-200': '#f2f3f4',
+			},
 		},
-		extend: {},
 	},
 	plugins: [],
 };
