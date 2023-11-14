7c6eb36373a09c15ef99319a9b1de609b3d8c5d6
Chaion Saha <chaionsaha00@gmail.com>
2023-11-14 11:02:05 +0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./**/*.{html,js}'],
+	theme: {
+		colors: {
+			primary: '#ef3b2e',
+			'parimary-subtile': '#ffccba',
+			'bg-base-100': '#fff',
+			'bg-base-200': '#f2f3f4',
+		},
+		extend: {},
+	},
+	plugins: [],
+};
